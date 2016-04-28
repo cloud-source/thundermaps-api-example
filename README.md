eb550cfdcd905ecfbb416539c169d002b98dc25e
Daniel Gibbs <daniel@danielgibbs.name>
2016-04-28 12:42:54 +1200

    Initial commit with Python example

---
+++ README.md
@@ -0,0 +1,18 @@
+ThunderMaps API Examples
+========================
+
+This repository contains examples in several different programming languages that show how to use the ThunderMaps API. To run the examples you will need to obtain a ThunderMaps API key and channel ID.
+
+API key
+-------
+
+To use any of these examples you will need a ThunderMaps API key. If you do not have one, you will need to create a ThunderMaps account at https://www.thundermaps.com/starts/new. After creating your account and logging in you will be able to find your API Key at https://www.thundermaps.com/api/keys/show.
+
+Once you have your API key you will need to replace any occurence of `YOUR API KEY HERE` in the examples with your API key.
+
+Channel ID
+----------
+
+In order to send a report you will need to create a channel to submit it to. You can do this at https://www.thundermaps.com/accounts/new.
+
+Once you have your channel ID you will need to replace any occurence of `YOUR CHANNEL ID HERE` in the examples with your channel ID.
---
+++ thundermaps.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+#
+# Example Python script that sends a report to ThunderMaps using the ThunderMaps API.
+#
+# This script requires the Requests library (http://www.python-requests.org/en/master/) but this can
+# be replaced by another HTTP library if needed.
+
+import json
+
+import requests
+
+THUNDERMAPS_API_KEY = "YOUR API KEY HERE"
+THUNDERMAPS_CHANNEL_ID = "YOUR CHANNEL ID HERE"
+
+class ThunderMaps(object):
+	def __init__(self, key, channel_id):
+		self.key = key
+		self.channel_id = channel_id
+
+	def send_report(self, report):
+		url = "https://www.thundermaps.com/api/v2/reports"
+		data = json.dumps({"reports": [report]})
+		params = {"account_id": self.channel_id}
+		headers = {"Authorization": "Token token=%s" % self.key, "Content-Type": "application/json"}
+		try:
+			response = requests.post(url, params=params, data=data, headers=headers)
+			response.raise_for_status()
+		except requests.exceptions.HTTPError as e:
+			raise Exception("Error sending report: received HTTP %d from server" % e.response.status_code)
+
+if __name__ == "__main__":
+	# Send a sample report to ThunderMaps.
+	thundermaps = ThunderMaps(THUNDERMAPS_API_KEY, THUNDERMAPS_CHANNEL_ID)
+	thundermaps.send_report({
+		"latitude": 33.747252,
+		"longitude": -112.633853,
+		"address": "N Ogden Rd, Wittmann, AZ 85361",
+		"title": "Giant Triangle",
+		"description": "A giant triangle in the desert of Arizona",
+		"category_name": "Shape",
+		"occurred_on": "2016-04-25T12:00:00Z",
+		"source_id": "triangle001",
+	})
